c384ac30e4975b2a7d1e1449de513bc20cf4980a
dalgonaio <8888888dalgonaio@users.noreply.github.com>
2021-10-07 20:18:22 -0400

    Feat: login screen upon start and color scheme of same

--- App.js
+++ App.js
@@ -1,17 +1,38 @@
 /* eslint-disable no-unused-vars */
 import 'react-native-gesture-handler';
-import { StatusBar } from 'expo-status-bar';
+import {StatusBar} from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
+import {StyleSheet, Text, View} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import LoginScreen from './screens/LoginScreen';
+
+const Stack = createNativeStackNavigator();
+
+const globalScreenOptions = {
+  headerStyle: {backgroundColor: '#718355'},
+  headerTitleStyle: {color: '#FFFFFF'},
+  //tint color of icons
+  headerTintColor: '#ebf7de',
+};
 
 export default function App() {
   return (
     <NavigationContainer>
-      <View style={styles.container}>
+      <Stack.Navigator screenOptions={globalScreenOptions}>
+        <Stack.Screen
+          options={{
+            title: 'Log In',
+          }}
+          name="Login"
+          component={LoginScreen}
+        />
+      </Stack.Navigator>
+
+      {/* <View style={styles.container}>
       <Text>Hello World-Lupin!</Text>
       <StatusBar style="auto" />
-    </View>
+    </View> */}
     </NavigationContainer>
   );
 }
---
+++ screens/LoginScreen.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const LoginScreen = () => {
+  return (
+    <View>
+      <Text>Meow Talk - Log In!</Text>
+    </View>
+  )
+}
+
+export default LoginScreen
+
