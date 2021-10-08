16f8c2feab8387f8e9e8ab025f002d876554de93
dalgonaio <8888888dalgonaio@users.noreply.github.com>
2021-10-07 22:08:14 -0400

    initialized firebase

--- firebase.js
+++ firebase.js
@@ -1,3 +1,7 @@
+/* eslint-disable no-unused-vars */
+import * as firebase from "firebase";
+import "firebase/auth";
+
 const firebaseConfig = {
   apiKey: "AIzaSyDsujUSleWYLpALXKZmI0h9ZAJLfXNOhwA",
   authDomain: "meowtalk-b16a3.firebaseapp.com",
@@ -6,3 +10,14 @@ const firebaseConfig = {
   messagingSenderId: "937916533462",
   appId: "1:937916533462:web:3ea9f2fa7b95d4d1059777"
 };
+
+let app;
+
+if (firebase.apps.length ===0 ) {
+  app = firebase.initializeApp(firebaseConfig);
+} else {
+  app = firebase.app();
+}
+
+const db = app.firestore();
+const auth = firebase.auth();
