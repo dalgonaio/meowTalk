2240731c22100ecc6a21746590ec6f26b4a2cca1
dalgonaio <8888888dalgonaio@users.noreply.github.com>
2021-10-07 22:15:05 -0400

    connected register screen to firebase auth

--- firebase.js
+++ firebase.js
@@ -20,4 +20,4 @@ if (firebase.apps.length ===0 ) {
 }
 
 const db = app.firestore();
-const auth = firebase.auth();
+export const auth = firebase.auth();
--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -3,6 +3,7 @@ import React, {useLayoutEffect, useState} from 'react';
 import {KeyboardAvoidingView, StyleSheet, View} from 'react-native';
 import {Button, Input, Text} from 'react-native-elements';
 import {StatusBar} from 'expo-status-bar';
+import {auth} from '../firebase'
 
 const RegisterScreen = ({navigation}) => {
   const [name, setName] = useState('');
@@ -16,7 +17,16 @@ const RegisterScreen = ({navigation}) => {
     })
   }, [navigation])
 
-  const submit = () => {}
+  const submit = () => {
+    auth.createUserWithEmailAndPassword(email, password)
+    .then(authUser => {
+      authUser.user.update({
+        displayName: name,
+        photoURL: imageUrl || "https://images.unsplash.com/photo-1561948955-570b270e7c36?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTIyfHxncnVtcHklMjBjYXR8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+      })
+    })
+    .catch(error => alert(error.message));
+  }
 
   return (
     <KeyboardAvoidingView behavior="padding" style={styles.container}>
