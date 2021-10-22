5438cd2599366ec8b3ff598ba2ab8f6ab74f5304
dalgonaio <8888888dalgonaio@users.noreply.github.com>
2021-10-22 10:35:29 -0400

    Feat: connect chat screen to db in sending message

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,12 +1,27 @@
 /* eslint-disable no-unused-vars */
 import React, {useLayoutEffect, useState} from 'react';
-import {KeyboardAvoidingView, Platform, SafeAreaView, ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View} from 'react-native';
+import {
+  Keyboard,
+  KeyboardAvoidingView,
+  Platform,
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  View,
+} from 'react-native';
 import {Avatar} from 'react-native-elements';
 import {AndDesign, FontAwesome, Ionicons} from '@expo/vector-icons';
-import { StatusBar } from 'expo-status-bar';
+import {StatusBar} from 'expo-status-bar';
+import { auth, db } from '../firebase';
+import * as firebase from "firebase"
+
 
 const ChatScreen = ({navigation, route}) => {
-  const [input, setInput] = useState("")
+  const [input, setInput] = useState('');
   useLayoutEffect(() => {
     navigation.setOptions({
       headerBackTitleVisible: false,
@@ -36,54 +51,91 @@ const ChatScreen = ({navigation, route}) => {
         </View>
       ),
       headerRight: () => (
-        <View style={{
-          flexDirection: "row",
-          justifyContent:"space-between",
-          width: 80,
-          marginRight: 20,
-        }}>
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            width: 80,
+            marginRight: 20,
+          }}
+        >
           <TouchableOpacity>
-          <FontAwesome name="video-camera" size={24} color="white" />
+            <FontAwesome name="video-camera" size={24} color="white" />
           </TouchableOpacity>
           <TouchableOpacity>
-          <Ionicons name="call" size={24} color="white" />
+            <Ionicons name="call" size={24} color="white" />
           </TouchableOpacity>
         </View>
       ),
     });
   }, [navigation]);
+
+  const sendMessage = () => {
+    Keyboard.dismiss();
+    db.collection("chats").doc(route.params.id)
+    .collection("messages")
+    .add({
+      timestamp:firebase.firestore.FieldValue.serverTimestamp(),
+      message: input,
+      displayName: auth.currentUser.displayName,
+      email: auth.currentUser.email,
+      photoURL: auth.currentUser.photoURL,
+    })
+
+    setInput('')
+  };
+
   return (
-      <SafeAreaView style={{flex:1, backgroundColor: "white"}}>
-        <StatusBar style="light" />
+    <SafeAreaView style={{flex: 1, backgroundColor: 'white'}}>
+      <StatusBar style="light" />
 
       <KeyboardAvoidingView
-      behavior = {Platform.OS === "ios"? "padding": "height"}
-      style={styles.container}
-      keyboardVerticalOffset={90}
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+        style={styles.container}
+        keyboardVerticalOffset={90}
       >
-<>
-<ScrollView>
-{/* Chat goes here */}
-</ScrollView>
-<View style={styles.footer} >
-  <TextInput
-  value={input}
-  onChangeText={(text)=>setInput(text)}
-  placeholder="meowTalk Message"
-   styles={styles.textInput} />
-  </View>
-</>
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <>
+          <ScrollView>{/* Chat goes here */}</ScrollView>
+          <View style={styles.footer}>
+            <TextInput
+              value={input}
+              onChangeText={(text) => setInput(text)}
+              onSubmitEditing={sendMessage}
+              placeholder="Your message here..."
+              styles={styles.textInput}
+            />
+            <TouchableOpacity onPress={sendMessage} activeOpacity={0.5}>
+              <Ionicons name="send" size={24} color="#718355" />
+            </TouchableOpacity>
+          </View>
+          </>
+        </TouchableWithoutFeedback>
       </KeyboardAvoidingView>
-
-      </SafeAreaView>
-
+    </SafeAreaView>
   );
 };
 
 export default ChatScreen;
 
 const styles = StyleSheet.create({
-  container: {},
-  footer: {},
-  textInput: {}
+  container: {
+    flex: 1,
+  },
+  footer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    width: '100%',
+    padding: 15,
+  },
+  textInput: {
+    bottom: 0,
+    height: 40,
+    flex: 1,
+    marginRight: 15,
+    backgroundColor: '#ECECEC',
+    padding: 10,
+    color: 'grey',
+    borderRadius: 30,
+  },
 });
